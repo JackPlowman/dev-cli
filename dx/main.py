31e47143a9a7d07ed13b0a480032e423f3c5b108
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-07-20 23:20:26 +0100

    Move state command to main.py (#14)
    # Pull Request
    
    ## Description
    
    This pull request refactors the `state` command by consolidating its
    implementation into the `dx/main.py` file and removing the redundant
    `dx/state.py` file.
    
    Codebase refactoring:
    
    *
    [`dx/main.py`](diffhunk://#diff-5fce7d475c8962de77305a6b981eca1f18b745a5ea0252f98bb94897ac0e6246R2-R17):
    Added the `state` command implementation, which commits changes with a
    specified message using the `git` library.
    *
    [`dx/state.py`](diffhunk://#diff-8bc448410f02ff50e1007f57ecacc8358d9ff46c1f25c51613877b2ac2628598L1-L15):
    Removed the file entirely, as its functionality has been merged into
    `dx/main.py`.

--- dx/main.py
+++ dx/main.py
@@ -1,6 +1,19 @@
 import typer
+from git import Repo
 
 app = typer.Typer()
+
+
+@app.command()
+def state(message: str) -> None:
+    """Commit changes with state message.
+
+    Args:
+        message (str): The message to commit with.
+    """
+    repo = Repo(".")
+    repo.index.commit(f"Add State for {message}")
+
 
 if __name__ == "__main__":
     app()
